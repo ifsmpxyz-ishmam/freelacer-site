7af8c5dd5019d8465a4f3ed204014d2d3f96364f
ifsmpxyz-ishmam <ifsmpxyz@gmail.com>
2026-08-28 17:24:04 +0600

    Update reviews.js

--- netlify/function/reviews.js
+++ netlify/function/reviews.js
@@ -1,1 +1,24 @@
+exports.handler = async (event, context) => {
+    const token = process.env.Reviews_API_KEY;
+    const baseId = process.env.Reviews_BASE_ID;
+    const tableName = "Freelancer Site Reviews";
+    const url = `https://api.airtable.com/v0/${baseId}/${tableName}?filterByFormula={Status}='Approved'`;
+
+try {
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+    
+    });
+    const responseData = await response.json();
+    return {
+        statusCode: 200,
+        body: JSON.stringify(responseData.records)
+    };
+} catch (error) {
+    console.error('Error submitting your review:', error);
+    return { statusCode: 500, body: JSON.stringify({ error: 'Failed to fetch data from Airtable' }) };
+}
+}
 
